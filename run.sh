90b73867a4384f9abe5fe800a2d2c4a39bbb0f21
Ubuntu <inverse-pandas@cs224n-assignments.vmpbzhk32j4udguysdjvsah4sd.bx.internal.cloudapp.net>
2019-03-17 21:12:00 +0000

    dev predictions!

--- run.sh
+++ run.sh
@@ -2,17 +2,15 @@ MODEL="bert-base-uncased"
 
 python ./examples/run_squad.py \
   --bert_model $MODEL \
-  --do_train \
   --do_predict \
   --do_lower_case \
-  --train_file ../squad/data/train-v2.0.json \
-  --predict_file ../squad/data/dev-v2.0.json \
+  --predict_file ../squad/data/test-v2.0.json \
   --learning_rate 3e-5 \
-  --num_train_epochs 5 \
+  --num_train_epochs 6 \
   --max_seq_length 384 \
   --doc_stride 128 \
   --output_dir ./save/$MODEL-5 \
-  --train_batch_size 32 \
+  --train_batch_size 48 \
   --gradient_accumulation_steps 2 \
   --version_2_with_negative \
   --null_score_diff_threshold 0.0
